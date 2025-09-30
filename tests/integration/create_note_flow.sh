d8c5be3d66157d5867aaeb84e035018ea63ebfea
Marco Velazquez <101480497+MarcoWrightk@users.noreply.github.com>
2025-09-29 22:25:24 -0600

    Update create_note_flow.sh

--- tests/integration/create_note_flow.sh
+++ tests/integration/create_note_flow.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+echo "Creating test note..."
+curl -X POST http://localhost:8080/api/notes/create \
+  -H "Content-Type: application/json" \
+  -d '{
+    "audioBase64": "dGVzdC1hdWRpbw==",
+    "sketchBase64": "dGVzdC1za2V0Y2g="
+  }'
+
+echo -e "\n\nFetching all notes..."
+curl -X GET http://localhost:8080/api/notes/all
 
